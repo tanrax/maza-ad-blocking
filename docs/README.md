5154e75cc3622f1cc2299ad531d2f87b6d69af5d
Andros Fenollosa <andros@fenollosa.email>
2020-04-10 13:55:02 +0200

    Add website

---
+++ docs/README.md
@@ -0,0 +1,14 @@
+# web-important-stories-alert-for-hacker-news
+
+## Make css
+
+```sh
+npm install tailwindcss
+npx tailwind build css/tailwinds/main.css -o css/tailwinds/dist.css
+```
+
+## Dev
+
+```sh
+parcel index.html
+```
---
+++ docs/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      display: ['Source Sans Pro', 'sans-serif'],
+      body: ['Martel Sans', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        orange: '#e95527',
+        bluedark: '#2364aa',
+        blue: '#3da5d9',
+        yellow: '#fec601'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
